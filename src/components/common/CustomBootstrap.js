8761386acbdff1b8c852a80ba57857952cf3ca6e
aly3n <aly3n.dev@gmail.com>
2021-05-10 05:19:43 +0430

    remove everything

--- src/components/common/CustomBootstrap.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Col } from 'reactstrap';
-const Colxx = (props) => (
-    <Col {...props} widths={['xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl']} />
-);
-const Separator = (props) => (
-    <div className={`separator ${props.className}`}></div>
-);
-export { Colxx,Separator }
--- src/views/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-
-class Main extends Component {
-  render() {
-    return <Redirect to="/app" />
-  }
-}
-export default Main;
